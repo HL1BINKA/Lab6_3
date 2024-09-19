19fdb3061d34befb981f6bdd2db208229d3d8169
Alexandr_Kisliy <sashakisliy2006@gmail.com>
2024-09-19 19:49:48 +0300

    git commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def add_vowels_to_set():
     digits_set = {'c', 'd'}
-    vowels_set = {'a', 'е', 'є', 'и', 'і', 'ї', 'о', 'у', 'ю', 'я'}
+    vowels_set = {'а', 'е', 'є', 'и', 'і', 'ї', 'о', 'у', 'ю', 'я'}
     result = digits_set.union(vowels_set)
-    print("Множина після додавання голосних:", result)
+    if isinstance(result, set):
+        print("Множина після додавання голосних:", result)
+    else:
+        result_list = list(digits_set) + list(vowels_set)
+        print("Перетворено на список і знову на множину:", set(result_list))
+
 add_vowels_to_set()
